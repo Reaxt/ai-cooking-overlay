3a48c7c6b909d44deb7d1dac423546b65a452c8f
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-06-20 16:24:32 -0700

    Types

---
+++ server/tiltify/donation.ts
@@ -0,0 +1,5 @@
+export type Donation = {
+    donator: string,
+    amount: number,
+}
+
---
+++ server/types.ts
@@ -0,0 +1,43 @@
+export type Donation = {
+    donator: string,
+    amount: number,
+}
+export type Reward = {
+    name: string,
+    description: string,
+    imagePath: string,
+    cost: number
+}
+export type Target = {
+    name: string,
+    cost: number,
+    amountRaised: number,
+    active: boolean,
+    achieved: boolean
+}
+export type Poll = {
+    name: string,
+    amountRaised: number,
+    active: boolean,
+    options: PollOption[]
+}
+export type PollOption = {
+    name: string, 
+    amountRaised: number
+}
+export type Milestone = {
+    name: string,
+    amount: number,
+    active: boolean,
+    achieved: boolean
+}
+export type Campaign = {
+    amount_raised: number,
+    end_goal: number,
+    milestones: Milestone[],
+    rewards: Reward[],
+    polls: Poll[],
+    donations: Donation[],
+    targets: Target[]
+
+}
